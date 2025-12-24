ad6f05fd3c9630062816d96dbe985db95cc8b97d
abdala hamwi <abdalah09941@gmail.com>
2025-12-24 12:19:53 +0300

    Implement book listing in frontend with DB integration

--- digital_library/models.py
+++ digital_library/models.py
@@ -12,16 +12,16 @@ class Category(models.Model):
 class Book(models.Model):
 
     status_book = [
-        ("مستاجر", "مسناجر"),
-        ("متاح", "متاح"),
-        ("مباع", "مباع"),
+        ("available", "available"),
+        ("rented", "rented"),
+        ("sold", "sold"),
     ]
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=50, null=True, blank=True)
     photo_book = models.ImageField(upload_to="photo", null=True, blank=True)
     photo_author = models.ImageField(upload_to="photo_author", null=True, blank=True)
     pages = models.IntegerField(null=True, blank=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=0, null=True, blank=True)
     rental_price_day = models.DecimalField(
         max_digits=5, decimal_places=2, null=True, blank=True
     )
