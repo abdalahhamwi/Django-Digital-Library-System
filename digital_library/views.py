f7d2f3ab320ddee7850ec5d33ef7e0abebf9bff3
abdala hamwi <abdalah09941@gmail.com>
2025-12-28 18:17:42 +0300

    add CategoryForm on side bar

--- digital_library/views.py
+++ digital_library/views.py
@@ -1,25 +1,56 @@
 from django.shortcuts import render
 from .models import *
+from .forms import BookForm, CategoryForm
 
 # Create your views here.
 
 
 def books(request):
+
+    if request.method == "POST":
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_category.save()
+
     return render(
         request,
         "books.html",
         {
             "books": Book.objects.all(),
             "cat": Category.objects.all(),
+            "form_category": CategoryForm(),
         },
     )
 
 
 def delete(request):
-    return render(request, "delete.html")
+
+    if request.method == "POST":
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_category.save()
+
+    return render(
+        request,
+        "delete.html",
+        {
+            "form_category": CategoryForm(),
+            "cat": Category.objects.all(),
+        },
+    )
 
 
 def index(request):
+    if request.method == "POST":
+        add_book = BookForm(request.POST, request.FILES)
+        if add_book.is_valid():
+            add_book.save()
+
+    if request.method == "POST":
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_category.save()
+
     return render(
         request,
         "index.html",
@@ -27,9 +58,24 @@ def index(request):
             "books": Book.objects.all(),
             "cat": Category.objects.all(),
             "num": str(Book.objects.count()),
+            "form": BookForm(),
+            "form_category": CategoryForm(),
         },
     )
 
 
 def update(request):
-    return render(request, "update.html")
+
+    if request.method == "POST":
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_category.save()
+
+    return render(
+        request,
+        "update.html",
+        {
+            "form_category": CategoryForm(),
+            "cat": Category.objects.all(),
+        },
+    )
