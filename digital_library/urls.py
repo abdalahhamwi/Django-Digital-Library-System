22ee60a6c3b7fe37347af8340b34bd5b95a6a03f
abdala hamwi <abdalah09941@gmail.com>
2025-12-30 15:57:31 +0300

    add form_book with CSRF and link books by id

--- digital_library/urls.py
+++ digital_library/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("books/", views.books, name="books"),
     path("delete/", views.delete, name="delete"),
     path("", views.index, name="index"),
-    path("update/", views.update, name="update"),
+    path('update/<int:id>/', views.update, name='update'),
+    
 ]
--- digital_library/views.py
+++ digital_library/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import BookForm, CategoryForm
 
@@ -64,12 +64,23 @@ def index(request):
     )
 
 
-def update(request):
+def update(request, id):
 
     if request.method == "POST":
         add_category = CategoryForm(request.POST)
         if add_category.is_valid():
             add_category.save()
+
+    book_id = Book.objects.get(id=id)
+
+    if request.method == "POST":
+        book_save = BookForm(request.POST, request.FILES, instance=book_id)
+        if book_save.is_valid():
+            book_save.save()
+            return redirect("/")
+
+    else:
+        book_save = BookForm(instance=book_id)
 
     return render(
         request,
@@ -77,5 +88,7 @@ def update(request):
         {
             "form_category": CategoryForm(),
             "cat": Category.objects.all(),
+            "form": BookForm(),
+            "form_book": book_save,
         },
     )
