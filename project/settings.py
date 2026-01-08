dd08bf9947cb67872e04342701ab3d2e317a2af4
abdala hamwi <abdalah09941@gmail.com>
2026-01-08 03:17:29 +0300

    fix staticfiles

--- project/settings.py
+++ project/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "")
+SECRET_KEY = "Msh6-H_F8K67ThDipuYNu7Rk-tTN9va9-rE5VHE9gJxEfS8Y234r_zYnav5Uvu5v_yk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG ="True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = []
 
 
 
